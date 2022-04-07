b857ff2edff54cffd95a732ca3f1a6a29a5d351b
adam.lai[賴奕牧] <adam.lai@104.com.tw>
2022-04-07 18:12:42 +0800

    拆開 Aged Brie || Backstage passes

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -12,21 +12,26 @@ class Shop {
   }
   updateQuality() {
     for (let item of this.items) {
-      if (item.name === 'Aged Brie' || item.name === 'Backstage passes to a TAFKAL80ETC concert') {   
+      if (item.name === 'Aged Brie') {
         if (item.quality < 50) {
           item.quality = item.quality + 1;
-          if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
+        }
+      } else if (item.name === 'Backstage passes to a TAFKAL80ETC concert') {
+        if (item.quality < 50) {
+          item.quality = item.quality + 1;
+        }
+        if (item.name === 'Backstage passes to a TAFKAL80ETC concert') {
             if (item.sellIn < 11) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1;
-              }
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+                }
             }
+
             if (item.sellIn < 6) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1;
-              }
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+                }
             }
-          }
         }
       } else {
         if (item.quality > 0) {
