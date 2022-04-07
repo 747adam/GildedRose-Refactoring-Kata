cd7842f36467dbee9ea01b3b09a85a2079fe0af8
adam.lai[賴奕牧] <adam.lai@104.com.tw>
2022-04-07 18:03:53 +0800

    安裝及寫測試

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -1,9 +1,51 @@
 const {Shop, Item} = require("../src/gilded_rose");
 
 describe("Gilded Rose", function() {
-  it("should foo", function() {
+  it("foo", function() {
     const gildedRose = new Shop([new Item("foo", 0, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
+    expect(items[0].quality).toBe(0);
+  });
+
+  it("Sulfuras, Hand of Ragnaros", function() {
+    const gildedRose = new Shop([new Item("Sulfuras, Hand of Ragnaros", 10, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(0);
+  });
+
+  it("Aged Brie", function() {
+    const gildedRose = new Shop([new Item("Aged Brie", 10, 50)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(50);
+  });
+
+  it("foo", function() {
+    const gildedRose = new Shop([new Item("foo", -1, 40)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(38);
+  });
+
+  it("Aged Brie", function() {
+    const gildedRose = new Shop([new Item("Aged Brie", 1, 40)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(41);
+  });
+
+  it("Aged Brie", function() {
+    const gildedRose = new Shop([new Item("Aged Brie", -1, 40)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(42);
+  });
+
+  it("Aged Brie", function() {
+    const gildedRose = new Shop([new Item("Aged Brie", -1, 50)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(50);
+  });
+
+  it("Backstage passes to a TAFKAL80ETC concert", function() {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 10, 40)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(42);
   });
 });
